8d87423b30456ed72bd45585bd3e67995ecc9cb9
edemdev7 <ekpomachi@gmail.com>
2025-08-13 22:20:59 +0100

    correction

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://192.168.1.15:8000',
+  baseURL: 'https://api.africarisque.com',
   headers: {
     'Content-Type': 'application/json',
   },
